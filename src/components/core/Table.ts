5fed23918c58232f3211156edd6fd896fd8bfa02
ismailuddin <ismail.sameeuddin@gmail.com>
2021-09-21 20:58:41 +0100

    :sparkles: Add Table component

---
+++ src/components/core/Table.ts
@@ -0,0 +1,49 @@
+import { ComponentExtendable, IComponentDefinition } from "../base";
+
+class Table extends ComponentExtendable {
+    title: string | null;
+    headings: Array<string>;
+    rows: Array<Array<string>>;
+    striped: boolean;
+
+    /**
+     * Table constructor
+     * @param config Component config
+     * @param config.title Table title
+     * @param config.headings Table headings
+     * @param config.rows Table rows
+     * @param config.striped Whether table is striped
+     */
+    constructor({
+        title,
+        headings,
+        rows,
+        striped
+    }: {
+        title: string,
+        headings: Array<string>,
+        rows: Array<Array<string>>,
+        striped: boolean,
+    }) {
+        super({ type: "Table" });
+        this.title = title;
+        this.headings = headings;
+        this.rows = rows;
+        this.striped = striped;
+    }
+
+    getDefinition(): IComponentDefinition {
+        return {
+            type: this.type,
+            options: {
+                title: this.title,
+                headings: this.headings,
+                rows: this.rows,
+                striped: this.striped,
+            },
+            components: this.components.map((c) => c.getDefinition()),
+        };
+    }
+}
+
+export { Table };
--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,2 +1,3 @@
 export { Row } from './Row';
 export { Section } from './Section';
+export { Table } from './Table';
