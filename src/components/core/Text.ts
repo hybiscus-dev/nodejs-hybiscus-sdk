ca981ac18d277a891f91956927f7372a7860d7b7
ismailuddin <ismail.sameeuddin@gmail.com>
2021-09-22 19:45:54 +0100

    :sparkles: Add Text component

---
+++ src/components/core/Text.ts
@@ -0,0 +1,39 @@
+import { ComponentExtendable, IComponentDefinition } from "../base";
+import { WidthType } from "../types";
+
+
+class Text extends ComponentExtendable {
+    text: string;
+    width: WidthType;
+
+    /**
+     * Text constructor
+     * @param config Component config
+     * @param config.text Text to add inside the component
+     * @param config.width Width of the component
+     */
+    constructor({
+        text,
+        width,
+    }: {
+        text: string;
+        width: WidthType;
+    }) {
+        super({ type: "Text" });
+        this.text = text;
+        this.width = width;
+    }
+
+    getDefinition(): IComponentDefinition {
+        return {
+            type: this.type,
+            options: {
+                text: this.text,
+                width: this.width,
+            },
+            components: this.components.map((c) => c.getDefinition()),
+        };
+    }
+}
+
+export { Text };
--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -3,3 +3,4 @@ export { Section } from "./Section";
 export { Table } from "./Table";
 export { Card } from "./Card";
 export { Image } from "./Image";
+export { Text } from "./Text";
